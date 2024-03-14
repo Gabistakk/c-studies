f6c634800cacc7bc614e398c5a8f4b58ca4c94a0
Gabistakk <gabistakk@gmail.com>
2024-03-14 17:22:13 -0300

    create read update finished

--- main.c
+++ main.c
@@ -1,22 +1,60 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 // set crud functions
-void create();
-void read();
-void update();
-void destroy();
+void create(char *value);
+void read(int position);
+void update(char *value, int position);
+void destroy(int position);
 
-// set array as tinydb
-char *crudArray[][255] = {};
+
+// set ultility function
+int checkIfPositionIsValid(int position);
+
+// set an array of pointers as tinydb
+#define MAX_ENTRIES 100
+char *crudArray[MAX_ENTRIES] = {};
+int count = 0;
 
 void main(){
 
-    *crudArray[0] = "teste";
+    destroy(2);
 
-    printf("%s \n", *crudArray[0]);
+   for(int i = 0; i < count; i++) read(i);
 }
 
-void create(){
-    
+void create(char *value){
+    crudArray[count++] = value;
+}
+
+void read(int position){
+    if(checkIfPositionIsValid(position) && crudArray[position] != NULL) printf("%s \n", crudArray[position]);
+}
+
+void update(char *value, int position){
+    if(checkIfPositionIsValid(position)) crudArray[position] = value;
+}
+
+void destroy(int position){
+    if(checkIfPositionIsValid(position)){
+        crudArray[position] = NULL;
+
+        if(position == count - 1){
+            count--;
+            return;
+        }
+
+        for(int i = count - 1; i >= position; i--){
+            
+        }
+    }
+}
+
+int checkIfPositionIsValid(int position){
+    if(position >= count){
+        printf("Invalid Position. \n");
+        return 0;
+    }
+    return 1;
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,2 @@
-for (int i = 0; i < 10; i++) dynamicArray_append(da, i);
+;
+            printf("%s \n", crudArray[i]);
