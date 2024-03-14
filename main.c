b07be0df23da6a4b05237df30143cf59dfa7774a
Gabistakk <gabistakk@gmail.com>
2024-03-14 01:10:35 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+// set crud functions
+void create();
+void read();
+void update();
+void destroy();
+
+// set array as tinydb
+int dinamicSize = 0;
+char *crudArray[dinamic] = {};
+
+void main(){
+
+    crudArray[0] = "teste";
+
+    printf("%s \n", *crudArray[0]);
+}
+
+void create(){
+    
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+
+// set array as tinydb
+char crudArray[][255] = {};
+
+void main(){
+      crudArray[0] = "teste";
