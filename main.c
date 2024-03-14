f7a917dbcbb7ed7c905aaa21c4f0ce4e56b0c52d
Gabistakk <gabistakk@gmail.com>
2024-03-14 20:03:36 -0300

    final version

--- main.c
+++ main.c
@@ -18,36 +18,48 @@ char *crudArray[MAX_ENTRIES] = {};
 int count = 0;
 
 void main(){
+    //create examples
+    create("test1");
+    create("test2");
+    create("test3");
+    create("test4");
+    create("test5");
 
-    destroy(2);
-
+    // final loop to exhibit all of the db content
    for(int i = 0; i < count; i++) read(i);
 }
 
 void create(char *value){
+    // create the element as it increases the counter of elements.
     crudArray[count++] = value;
 }
 
 void read(int position){
-    if(checkIfPositionIsValid(position) && crudArray[position] != NULL) printf("%s \n", crudArray[position]);
+    // prints the element.
+    if(checkIfPositionIsValid(position)) printf("%s \n", crudArray[position]);
 }
 
 void update(char *value, int position){
+    // pretty self explanatory, but we substitute the data of the element with the target value.
     if(checkIfPositionIsValid(position)) crudArray[position] = value;
 }
 
 void destroy(int position){
     if(checkIfPositionIsValid(position)){
         crudArray[position] = NULL;
-
+        
+        // if the destroy target is the last element in the array, it simply removes one from the counter of elements.
         if(position == count - 1){
             count--;
             return;
         }
 
-        for(int i = count - 1; i >= position; i--){
-            
+        // if it isn't, then we shift all of the elements after the target to the left, filling it's gap.
+        for(int i = position; i <= count; i++){
+            crudArray[i] = crudArray[i + 1];
         }
+        // then of course mark it as one less element.
+        count--;
     }
 }
 
